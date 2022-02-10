75ac3a8e5e74d58c0325f3b8ef0f521fad641863
aster <aster@vers.site>
2022-02-10 00:12:07 +0000

    ✨ Wire iris CLI generate command and project schema loading

--- projects/iris.ts/iris/src/node/create-cli.ts
+++ projects/iris.ts/iris/src/node/create-cli.ts
@@ -1,11 +1,17 @@
 /**
  * Iris CLI (`cac`) — Node-only; bin entry is `src/node/cli.ts`.
  */
+import { readFile } from "node:fs/promises";
+import { resolve } from "node:path";
+
 import cac from "cac";
 
 import { version } from "../types/version.ts";
 import { checkSchemaFile } from "./check.ts";
 import { printDoctorReport } from "./doctor.ts";
+import { generateTypescriptClient } from "./generate-typescript.ts";
+import { loadSemanticCore } from "./native.ts";
+import { loadProject, readProjectSchema } from "./project.ts";
 
 function notImplemented(name: string): void {
     console.error(`iris ${name}: not implemented in @yydb/iris/node yet — use Rust iris-tools for full semantics`);
@@ -19,8 +25,8 @@ export function createIrisCli() {
     cli.version(version);
     cli.help();
 
-    cli.command("check [schema]", "Parse and validate a .iris schema")
-        .option("--config <path>", "Path to iris.von (ignored until project-aware check)")
+    cli.command("check [schema]", "Validate schema + generated client drift")
+        .option("--config <path>", "Path to iris.von")
         .action(async (schema?: string) => {
             if (!schema) {
                 console.error("iris check: schema path required");
@@ -30,12 +36,77 @@ export function createIrisCli() {
             process.exitCode = await checkSchemaFile(schema);
         });
 
-    cli.command("generate [schema]", "Generate host bindings via Dejavu")
+    cli.command("generate [schema]", "Generate TypeScript Iris client from .iris schema")
         .option("--config <path>", "Path to iris.von")
-        .option("--out <dir>", "Output directory")
+        .option("--out <dir>", "Project root for generated/iris output", { default: "." })
         .option("--target <name>", "Emitter target", { default: "typescript" })
-        .action((_schema?: string) => {
-            notImplemented("generate");
+        .action(async (schema?: string, options?: { out?: string; target?: string; config?: string }) => {
+            const target = options?.target ?? "typescript";
+            if (target === "rust") {
+                try {
+                    const core = await loadSemanticCore();
+                    const source = schema
+                        ? await readFile(resolve(schema), "utf8")
+                        : await readProjectSchema(await loadProject(options?.config ?? process.cwd()));
+                    const result = core.generateRust(source, resolve(options?.out ?? "generated"));
+                    if (!result.ok) {
+                        console.error(`error: ${result.error ?? "generate failed"}`);
+                        process.exitCode = 1;
+                        return;
+                    }
+                    console.log(`generated ${result.outputPath} (fingerprint=${result.schemaFingerprint})`);
+                } catch (error) {
+                    console.error(`error: ${error instanceof Error ? error.message : String(error)}`);
+                    process.exitCode = 1;
+                }
+                return;
+            }
+            if (target !== "typescript") {
+                console.error(`iris generate: unsupported target ${target} (use typescript or rust)`);
+                process.exitCode = 1;
+                return;
+            }
+            try {
+                const source = schema
+                    ? await readFile(resolve(schema), "utf8")
+                    : await readProjectSchema(await loadProject(options?.config ?? process.cwd()));
+                const result = await generateTypescriptClient(source, resolve(options?.out ?? "."));
+                console.log(
+                    `generated TypeScript client (${result.files.length} files, fingerprint=${result.introspection.schemaFingerprint})`,
+                );
+                for (const file of result.files) {
+                    console.log(`  - ${file}`);
+                }
+            } catch (error) {
+                console.error(`error: ${error instanceof Error ? error.message : String(error)}`);
+                process.exitCode = 1;
+            }
+        });
+
+    cli.command("push", "Push local schema to datasource (schema -> database)")
+        .option("--config <path>", "Path to iris.von")
+        .option("--source <name>", "Datasource name", { default: "default" })
+        .option("--out <dir>", "Output directory for push plan artifacts")
+        .option("--plan", "Plan only (no apply)")
+        .action(async (options?: { config?: string; source?: string; out?: string; plan?: boolean }) => {
+            const config = resolve(options?.config ?? "iris.von");
+            try {
+                const core = await loadSemanticCore();
+                if (options?.plan) {
+                    const result = core.migratePlanCmd(config, options?.source ?? "default", options?.out ?? null);
+                    if (!result.ok) {
+                        console.error(`error: ${result.error ?? "push plan failed"}`);
+                        process.exitCode = 1;
+                        return;
+                    }
+                    console.log(`push plan written: ${result.planPath}`);
+                    return;
+                }
+                notImplemented("push apply");
+            } catch (error) {
+                console.error(`error: ${error instanceof Error ? error.message : String(error)}`);
+                process.exitCode = 1;
+            }
         });
 
     cli.command("doctor", "Local diagnostics (config / environment)")
--- projects/iris.ts/iris/src/node/project.ts
+++ projects/iris.ts/iris/src/node/project.ts
@@ -1,18 +1,33 @@
-import { access } from "node:fs/promises";
 import { resolve } from "node:path";
 
 import { IrisFacadeError } from "../types/errors.ts";
+import { loadSemanticCore } from "./native.ts";
 
 const PROJECT_FILE = "iris.von";
 
 /** Load an on-disk Iris project directory (Node-only). */
-export async function loadProject(projectPath: string): Promise<{ readonly root: string; readonly config: string }> {
+export async function loadProject(projectPath: string): Promise<{ readonly root: string; readonly config: string; readonly schemaGlob: string }> {
+    const core = await loadSemanticCore();
     const root = resolve(projectPath);
-    const config = resolve(root, PROJECT_FILE);
+    const config = root.endsWith(PROJECT_FILE) ? root : resolve(root, PROJECT_FILE);
     try {
-        await access(config);
-    } catch {
-        throw new IrisFacadeError("project-missing", `@yydb/iris/node: ${PROJECT_FILE} not found under ${root}`);
+        const loaded = core.loadProject(config);
+        return {
+            root: loaded.root,
+            config: loaded.config,
+            schemaGlob: loaded.schemaGlob,
+        };
+    } catch (error) {
+        const message = error instanceof Error ? error.message : String(error);
+        if (message.includes("not found") || message.includes("No such file")) {
+            throw new IrisFacadeError("project-missing", `@yydb/iris/node: ${PROJECT_FILE} not found under ${root}`);
+        }
+        throw new IrisFacadeError("project-load-failed", `@yydb/iris/node: ${message}`);
     }
-    throw new IrisFacadeError("project-not-implemented", `@yydb/iris/node: project loader not implemented yet (${config})`);
+}
+
+/** Read merged schema text for a loaded project (Node-only). */
+export async function readProjectSchema(project: { root: string; schemaGlob: string }): Promise<string> {
+    const core = await loadSemanticCore();
+    return core.readSchema(project.root, project.schemaGlob);
 }
